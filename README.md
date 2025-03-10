250d603daa42f39fb18796043efb702ed552502f
Xunta <nameerror266@gmail.com>
2025-03-10 12:46:35 +0200

    Added New confs

--- README.md
+++
@@ -1,3 +0,0 @@
-## My Hyprland DotFiles
-### in 'More' Catppuccin theme
-### that's all
--- configs/waybar/restart.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-# Restart Waybar and swaync
-
-killall waybar
-killall swaync
-waybar &
-swaync &
-notify-send --app-name=HOME -i ~/.config/assets/kitty.png Hello
